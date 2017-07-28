fba84e06ef325853478e57314cda8be80ede3a6c
dwqs <461147874@qq.com>
2017-07-28 12:57:15 +0800

    export & tslint disable for some lines

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// tslint:disable-next-line:no-unused-variable
+import Vue from 'vue';
+
+// tslint:disable-next-line:no-unused-variable
+import { install, Config } from './install';
+import connect from './connect';
+
+const VueMobx = {
+    install,
+    connect,
+}
+
+export default VueMobx;
