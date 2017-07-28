9c21969bab13986c563f8b5325354b6ccf6a8f50
dwqs <461147874@qq.com>
2017-07-28 13:50:26 +0800

    update parameter type

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -1,5 +1,5 @@
 export declare function isObject(data: any): boolean;
 export declare function assert(condition: boolean, msg: string): void;
-export declare function getValidModel(states: any): object;
+export declare function getValidModel(states: object): object;
 export declare function getValidAction(actions: object): object;
 export declare function getMobxData(models: object): object;
