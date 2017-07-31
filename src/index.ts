99ecd2f1bf4c2f9b1b4ef8af4a0687b83c754fe1
dwqs <461147874@qq.com>
2017-07-31 23:08:25 +0800

    fix VueMobx export for typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 // tslint:disable-next-line:no-unused-variable
-import Vue from 'vue';
+import Vue, {PluginObject} from 'vue';
 
 // tslint:disable-next-line:no-unused-variable
-import { install, Config } from './install';
-import connect from './connect';
+import { install } from './install';
+import { connect } from './connect';
+import { Config } from './options';
 
-const VueMobx = {
+const VueMobx: PluginObject<Config> = {
     install,
     connect,
 }
